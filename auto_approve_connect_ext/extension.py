0c138b2a073a35160f11787bc06b200b669edb9a
Vladimir Timofeenko <id@vtimofeenko.com>
2021-10-31 18:41:10 -0700

    refactor: removed process_all_old_requests
    This code is not called by any current logic, removing.
    
    For historic reasons, could be retrieved from git history if needed.
    
    This change also removes dependency on toolz.
    
    Closes #2

--- auto_approve_connect_ext/extension.py
+++ auto_approve_connect_ext/extension.py
@@ -5,7 +5,6 @@
 #
 from connect.eaas.extension import Extension, ProcessingResponse, CustomEventResponse
 from uuid import uuid4
-from toolz import compose, pluck
 
 from threading import Thread
 
@@ -17,19 +16,6 @@ class AutoApprovalExtensionExtension(Extension):
         await self.client.requests[request_id].update(
             payload={"asset": {"params": [{"id": "volume_license", "value": str(license_key)}]}}
         )
-
-    async def process_all_old_requests(self):
-        """Kept as a reference only"""
-        requests = self.client.requests
-        for request_id in pluck(
-            "id",
-            requests.filter(asset__product__id="PRD-620-226-877")
-            .filter(type="purchase")
-            .filter(status="pending")
-            .all(),
-        ):
-            self.logger.info(f"Found old stale request {request_id}, processing it")
-            await self._set_license_on_request(request_id)
 
     async def process_asset_purchase_request(self, request):
         self.logger.info(f"Obtained request with id {request['id']}")
